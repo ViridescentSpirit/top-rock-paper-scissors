116aac7003c2d49b0e75f7fcb159a948397be552
Lane Bye <viridescentspiritgaming@gmail.com>
2023-11-28 08:58:27 -0600

    Create function for computer choice
    Created and tested function that will randomly select the computers
    choice for the rock, paper, scissors game. Confirmed that all three
    responses can and do occur via the console. Probably could have written
    the same thing with less code, but this made sure that my function was
    only doing one thing, and not trying to assign the value and randomize
    at the same time.

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 //Declare variables for computer and player choices
-//Determine the computers selection using random function
+let playerSelection = "None";
+let computerSelection = "None";
+//Determine the computers selection using random function to define option between 1-3
+function getComputerChoice() {
+    return Math.floor(Math.random()*3) + 1;
+}
+
+let computerRandomChoice = getComputerChoice();
+
+switch (computerRandomChoice) {
+    case 1:
+        computerSelection = "Rock";
+        break;
+    case 2:
+        computerSelection = "Paper";
+        break;
+    case 3:
+        computerSelection = "Scissors";
+        break;
+}
+
+console.log(computerRandomChoice);
+console.log(computerSelection);
 //Determine the players selection by collecting an input
 //Compare the computers selection and the players selection to determine a winner
 //If players select the same choice, replay the round until a winner is determined
