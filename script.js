8eaf9c3048b5915166d6c5958968f93ef6b45301
Lane Bye <viridescentspiritgaming@gmail.com>
2023-11-28 09:45:57 -0600

    Write game and collect result
    Wrote the code to run the actual game after collecting the user input.
    Currently just output the the result to be used in a game function that
    will count wins and losses.

--- script.js
+++ script.js
@@ -3,35 +3,51 @@ let playerSelection = "None";
 let computerSelection = "None";
 //Determine the computers selection using random function to define option between 1-3
 function getComputerChoice() {
-    return Math.floor(Math.random()*3) + 1;
+    computerRandomChoice = Math.floor(Math.random()*3) + 1;
+
+    switch (computerRandomChoice) {
+        case 1:
+            computerSelection = "rock";
+            break;
+        case 2:
+            computerSelection = "paper";
+            break;
+        case 3:
+            computerSelection = "scissors";
+            break;
+    }
 }
 
-let computerRandomChoice = getComputerChoice();
-
-switch (computerRandomChoice) {
-    case 1:
-        computerSelection = "rock";
-        break;
-    case 2:
-        computerSelection = "paper";
-        break;
-    case 3:
-        computerSelection = "scissors";
-        break;
-}
-
-console.log(computerRandomChoice);
-console.log(computerSelection);
+getComputerChoice();
 //Determine the players selection by collecting an input
 function getPlayerChoice() {
     return prompt("Rock, Paper, or Scissors?")
 }
 
 playerSelection = getPlayerChoice();
-console.log(playerSelection);
 //guarantee that the player selection will match case with the computer selection regardless
 //of actual user input
 playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection);
+console.log("Computer Chose: " + computerSelection);
+console.log("Player Chose: " + playerSelection);
 //Compare the computers selection and the players selection to determine a winner
+let gameResult = "None";
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("Tie! Replay round!");
+        return gameResult = "tie";
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+                (playerSelection === "scissors" && computerSelection === "paper") ||
+                (playerSelection === "paper" && computerSelection === "rock") ) {
+        console.log("Player Wins! Good Job!");
+        return gameResult = "win";
+    } else {
+        console.log("Computer wins, you're an idiot!");
+        return gameResult = "loss";
+    }
+}
+
+gameResult = playRound(playerSelection, computerSelection);
+console.log(gameResult);
 //If players select the same choice, replay the round until a winner is determined
