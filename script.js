eea3cb1214eef9cb7e332b4e613634d146e114a7
Lane Bye <viridescentspiritgaming@gmail.com>
2023-11-28 08:45:58 -0600

    Create script.js file and write pseudocode
    Changed my mind and decided to create the script in a separate file
    as that is best practice. Created the script.js file and wrote out
    rough pseudocode to provide a roadmap of what I want the code to
    accomplish.

---
+++ script.js
@@ -0,0 +1,5 @@
+//Declare variables for computer and player choices
+//Determine the computers selection using random function
+//Determine the players selection by collecting an input
+//Compare the computers selection and the players selection to determine a winner
+//If players select the same choice, replay the round until a winner is determined
